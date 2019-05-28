a2ff14449543e35ab1c99cf65ace3e4b59a3a824
Qunsong Fan (qufan) <qufan@cisco.com>
2019-05-28 13:30:28 +0800

    code

---
+++ src/main/java/jianzhioffer/tree/PostOrderUnRec.java
@@ -0,0 +1,33 @@
+package jianzhioffer.tree;
+
+import jianzhioffer.arraylist.ListNode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PostOrderUnRec {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.add(root);
+        while (!stack1.isEmpty()){
+            TreeNode tmp = stack1.pop();
+            stack2.add(tmp);
+            if(tmp.left!=null){
+                stack1.add(tmp.left);
+            }
+            if(tmp.right!=null){
+                stack1.add(tmp.right);
+            }
+
+        }
+        while (!stack2.isEmpty()){
+            list.add(stack2.pop().val);
+        }
+        return list;
+    }
+}
