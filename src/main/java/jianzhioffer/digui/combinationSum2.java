a0a38f983d3b2f36bf086a65e80569e257775306
Qunsong Fan (qufan) <qufan@cisco.com>
2019-08-07 17:43:16 +0800

    code

--- src/main/java/jianzhioffer/digui/combinationSum2.java
+++ src/main/java/jianzhioffer/digui/combinationSum2.java
@@ -1,18 +1,18 @@
 package jianzhioffer.digui;
 
-//https://leetcode-cn.com/problems/combination-sum/        39
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class combinationSum {
+//https://leetcode-cn.com/problems/combination-sum-ii/submissions/      40
+
+public class combinationSum2 {
     List<List<Integer>> lists = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         if(candidates == null || candidates.length == 0 || target < 0){
             return lists;
         }
-
+        Arrays.sort(candidates);
         List<Integer> list = new ArrayList<>();
         process(0,candidates,target,list);
         return lists;
@@ -26,16 +26,16 @@ public class combinationSum {
             lists.add(new ArrayList<>(list));
         }else {
             for (int i = start; i < candidates.length; i++) {
+                if(i > start && candidates[i] == candidates[i-1]) {
+                    continue;
+
+                }
+
                 list.add(candidates[i]);
-                process(i,candidates,target-candidates[i],list);
+                process(i+1,candidates,target-candidates[i],list);
                 list.remove(list.size()-1);
             }
         }
 
     }
-
-
-
-
-
 }
