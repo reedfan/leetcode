f2939b359fae48b116a8863b01bd3a4299d8f483
Qunsong Fan (qufan) <qufan@cisco.com>
2019-08-21 20:36:34 +0800

    code

---
+++ src/main/java/jianzhioffer/dp/NumDecodings91.java
@@ -0,0 +1,86 @@
+package jianzhioffer.dp;
+
+/**
+ * 一条包含字母 A-Z 的消息通过以下方式进行了编码：
+ * <p>
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * <p>
+ * 给定一个只包含数字的非空字符串，请计算解码方法的总数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "12"
+ * 输出: 2
+ * 解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: "226"
+ * 输出: 3
+ * 解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+ * <p>
+ * 来源：力扣（LeetCode）  91
+ * 链接：https://leetcode-cn.com/problems/decode-ways
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumDecodings91 {
+
+
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[len] = 1;
+        if (s.charAt(len - 1) == '0') {
+            dp[len - 1] = 0;
+        } else {
+            dp[len - 1] = 1;
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            if ((s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0') <= 26) {
+                dp[i] = dp[i + 1] + dp[i + 2];
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
+        return dp[0];
+    }
+
+    /*public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        return digui(s, 0);
+    }
+
+    private int digui(String s, int start) {
+        if (s.length() == start) {
+            return 1;
+        }
+        if (s.charAt(start) == '0') {
+            return 0;
+        }
+        int ans1 = digui(s, start + 1);
+
+        int ans2 = 0;
+
+        if (start < s.length() - 1) {
+            int ten = (s.charAt(start) - '0') * 10;
+            int one = (s.charAt(start + 1) - '0');
+            if (ten + one <= 26) {
+                ans2 = digui(s, start + 2);
+            }
+        }
+        return ans1 + ans2;
+
+    }*/
+}
---
+++ src/main/java/jianzhioffer/erfen/Search33.java
@@ -0,0 +1,62 @@
+package jianzhioffer.erfen;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * <p>
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * <p>
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * <p>
+ * 你可以假设数组中不存在重复的元素。
+ * <p>
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ * <p>
+ * 来源：力扣（LeetCode） 33
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Search33 {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //前半部分有序,注意此处用小于等于
+            if (nums[start] <= nums[mid]) {
+                //target在前半部分
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+
+    }
+
+}
