ed81d3e6f4923d276a6af82d87ad343005bd4ed2
reedfan <1617950759@qq.com>
2019-08-22 23:55:10 +0800

    leetcode

---
+++ src/main/java/jianzhioffer/tree/BuildTree106.java
@@ -0,0 +1,33 @@
+package jianzhioffer.tree;
+
+/**
+ * 根据一棵树的中序遍历与后序遍历构造二叉树。
+
+ 注意:
+ 你可以假设树中没有重复的元素。
+
+ 例如，给出
+
+ 中序遍历 inorder = [9,3,15,20,7]
+ 后序遍历 postorder = [9,15,7,20,3]
+
+ 返回如下的二叉树：
+
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+
+ 来源：力扣（LeetCode） 106
+ 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BuildTree106 {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+
+        return null;
+
+    }
+}
