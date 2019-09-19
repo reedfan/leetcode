92df4f18783700be5d404ba26f51f450696c81d1
reedfan <1617950759@qq.com>
2019-09-19 23:30:54 +0800

    leetcode

---
+++ src/main/java/leetcode/math/N292canWinNim.java
@@ -0,0 +1,26 @@
+package leetcode.math;
+
+/**
+ * 你和你的朋友，两个人一起玩 Nim 游戏：桌子上有一堆石头，每次你们轮流拿掉 1 - 3 块石头。 拿掉最后一块石头的人就是获胜者。你作为先手。
+ * <p>
+ * 你们是聪明人，每一步都是最优解。 编写一个函数，来判断你是否可以在给定石头数量的情况下赢得游戏。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 4
+ * 输出: false
+ * 解释: 如果堆中有 4 块石头，那么你永远不会赢得比赛；
+ * 因为无论你拿走 1 块、2 块 还是 3 块石头，最后一块石头总是会被你的朋友拿走。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/nim-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class N292canWinNim {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/leetcode/shuangzhizhen/N344reverseString.java
@@ -0,0 +1,41 @@
+package leetcode.shuangzhizhen;
+
+/**
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ * <p>
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * <p>
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class N344reverseString {
+    public void reverseString(char[] s) {
+        if (s == null || s.length < 2) {
+            return;
+        }
+        int left = -1;
+        int right = s.length;
+        while (++left < --right) {
+            char c = s[left];
+            s[left] = s[right];
+            s[right] = c;
+        }
+
+        return;
+    }
+}
