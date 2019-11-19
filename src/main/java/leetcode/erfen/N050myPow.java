63970560f148a05b535d3b2d1de38fbafab3b124
reedfan <1617950759@qq.com>
2019-11-20 00:58:24 +0800

    leetcode

---
+++ src/main/java/leetcode/erfen/N050myPow.java
@@ -0,0 +1,41 @@
+package leetcode.erfen;
+
+/**
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2.00000, 10
+ * 输出: 1024.00000
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: 2.10000, 3
+ * 输出: 9.26100
+ * <p>
+ * 示例 3:
+ * <p>
+ * 输入: 2.00000, -2
+ * 输出: 0.25000
+ * 解释: 2-2 = 1/22 = 1/4 = 0.25
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/powx-n
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class N050myPow {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n == -1) {
+            return 1 / x;
+        }
+        double half = myPow(x, n / 2);
+        double rest = myPow(x, n % 2);
+        return half * half * rest;
+    }
+}
