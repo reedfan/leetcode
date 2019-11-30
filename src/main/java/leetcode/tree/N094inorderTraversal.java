e9ac83c74ef32247db8d6bc0bb6da03f12b383ae
Qunsong Fan (qufan) <qufan@cisco.com>
2019-11-30 20:08:29 +0800

    code

--- src/main/java/leetcode/tree/N094inorderTraversal.java
+++ src/main/java/leetcode/tree/N094inorderTraversal.java
@@ -5,9 +5,16 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * created by reedfan on 2019/11/28 0028
+ * 非递归中序遍历二叉树
  */
 public class N094inorderTraversal {
+
+    /*
+    1.申请一个新的栈stack。
+    2.对于以root为根节点的这棵树，依次将左边界压入栈中。即不停的让root = root.left，不断重复此步骤
+    3.不断的重复2，直到发现cur为空，此时从stack中弹出一个节点。将该节点的值加入返回结果。并且让node = node.right然后重复步骤2.
+     */
+
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
 
@@ -15,8 +22,6 @@ public class N094inorderTraversal {
             return list;
         }
         Stack<TreeNode> stack = new Stack<>();
-
-
         while (!stack.empty() || root != null) {
 
             if (root != null) {
@@ -28,8 +33,6 @@ public class N094inorderTraversal {
                 root = root.right;
 
             }
-
-
         }
         return list;
 
