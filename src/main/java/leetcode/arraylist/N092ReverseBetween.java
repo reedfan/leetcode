42d63f956340e282390ed44e9f032d3c4c23279d
reedfan <1617950759@qq.com>
2019-12-17 23:51:23 +0800

    leetcode

--- src/main/java/leetcode/arraylist/N092ReverseBetween.java
+++ src/main/java/leetcode/arraylist/N092ReverseBetween.java
@@ -28,8 +28,14 @@ public class N092ReverseBetween {
         System.out.println(new N092ReverseBetween().reverseBetween(listNode1, 2, 5));
     }
 
-    public ListNode reverseBetween(ListNode head, int m, int n) {
+    /*
+    第一步：找到待反转节点的前一个节点
+    第二步：反转m到n这部分
+    第三步：将反转的起点的next指向反转的后面一部分。
+    第四步：将第一步找到的节点指向反转以后的头节点。
+     */
 
+    public ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode res = new ListNode(0);
         res.next = head;
         ListNode node = res;
@@ -37,27 +43,21 @@ public class N092ReverseBetween {
         for (int i = 1; i < m; i++) {
             node = node.next;
         }
-
         //node.next就是需要反转的这段的起点。
         ListNode nextHead = node.next;
         ListNode next = null;
-
         ListNode pre = null;
-
-        //反转n到m这一段
-        for (int i = 0; i <= n - m; i++) {
+        //反转m到n这一段
+        for (int i = m; i <= n; i++) {
             next = nextHead.next;
             nextHead.next = pre;
             pre = nextHead;
             nextHead = next;
         }
+        //将反转的起点的next指向next。
+        node.next.next = next;
         //需要反转的那一段的上一个节点的next节点指向反转后链表的头结点
         node.next = pre;
-        //找到反转这段的最后一个节点。
-        while (pre.next != null) {
-            pre = pre.next;
-        }
-        pre.next = next;
         return res.next;
     }
 }
