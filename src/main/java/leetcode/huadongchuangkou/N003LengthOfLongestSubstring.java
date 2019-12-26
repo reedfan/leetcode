185b060ea88a4b542bc49b356fad4fa8e6a56220
Qunsong Fan (qufan) <qufan@cisco.com>
2019-12-26 20:54:40 +0800

    code

--- src/main/java/leetcode/huadongchuangkou/N003LengthOfLongestSubstring.java
+++ src/main/java/leetcode/huadongchuangkou/N003LengthOfLongestSubstring.java
@@ -35,10 +35,28 @@ public class N003LengthOfLongestSubstring {
 
 
     public static void main(String[] args) {
-        System.out.println(new N003LengthOfLongestSubstring().lengthOfLongestSubstring("abcabcbb"));
-        Lock lock = new ReentrantLock(true);
+        System.out.println(new N003LengthOfLongestSubstring().lengthOfLongestSubstring("abcd"));
+
     }
 
+    /*
+    本题是一个典型的滑动窗口类题目，要求滑动窗口里没有重复的字符。
+    那么怎么滑动呢？
+    首先，我们需要用一个数据结构来记录窗口中的字符（可以是数组或者map，这个具体代码的时候再说）。
+    何时窗口右移？如果要处理的那个字符在滑动窗口里没有，那么窗口就可以右移了。因为此时还是能保证窗口里没有重复的字符。
+    窗口何时左移？如果要处理的那个字符在滑动窗口里有，那么窗口就需要左移。
+    左移什么时候结束？当左移到滑动窗口里面已经没有要处理的那个字符就可以结束了。
+    什么时候无重复子串最长？窗口最大的时候最长。
+
+    因此，回到刚才用来记录的数据结构那个问题，如果用数组怎么做？用数组的话，可以申请一个大小为256的数组，
+    当某个字符被加进滑动窗口，其对应位置的数就加1，当某个字符被移除滑动窗口，其对应位置的数就减1.
+    什么时候代表滑动窗口里面有这个字符？当其对应位置的值不为0就代表滑动窗口里面有这个字符了。
+
+    如果用map怎么做？可以用Map<Character, Integer> map来存储窗口中的值，key表示存储的字符，value表示存储的字符在已经遍历到的最右边的位置。
+    用l表示滑动窗口的左端。
+    当map里包含要处理的字符的时候，此时窗口的最左边应该是l和map.get(c) + 1的较大者。
+
+     */
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) {
             return 0;
@@ -76,7 +94,7 @@ public class N003LengthOfLongestSubstring {
         for (int i = 0; i < s.length(); i++) {
             Character c = s.charAt(i);
             if (map.containsKey(c)) {
-                l = map.get(c) + 1;
+                l = l > (map.get(c) + 1) ? l : (map.get(c) + 1);
             }
             map.put(c, i);
             maxLen = maxLen > (i - l + 1) ? maxLen : (i - l + 1);
