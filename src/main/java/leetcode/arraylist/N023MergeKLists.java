fd321ec11cad5b2c366b374f675ba04127a838fb
Qunsong Fan (qufan) <qufan@cisco.com>
2020-01-14 16:50:01 +0800

    digui

--- src/main/java/leetcode/arraylist/N023MergeKLists.java
+++ src/main/java/leetcode/arraylist/N023MergeKLists.java
@@ -1,5 +1,7 @@
 package leetcode.arraylist;
 
+import leetcode.tree.TreeNode;
+
 /**
  * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
  * <p>
@@ -31,10 +33,11 @@ public class N023MergeKLists {
         ListNode listNode3 = new ListNode(2);
         listNode3.next = new ListNode(6);
 
-        ListNode[] lists = {listNode1,listNode2,listNode3};
+        ListNode[] lists = {listNode1, listNode2, listNode3};
         new N023MergeKLists().mergeKLists(lists);
 
     }
+
     public ListNode mergeKLists(ListNode[] lists) {
         ListNode res = new ListNode(-1);
         if (lists == null || lists.length == 0) {
@@ -46,7 +49,7 @@ public class N023MergeKLists {
         int cntTmp = 0;
         ListNode head = res;
 
-        while (cntTmp < lists.length){
+        while (cntTmp < lists.length) {
             for (int i = 0; i < lists.length; i++) {
                 if (!tmp[i]) {
                     if (lists[i] == null) {
@@ -61,7 +64,7 @@ public class N023MergeKLists {
 
                 }
             }
-            if( min != Integer.MAX_VALUE){
+            if (min != Integer.MAX_VALUE) {
                 res.next = lists[minPos];
                 res = res.next;
                 lists[minPos] = lists[minPos].next;
@@ -74,25 +77,44 @@ public class N023MergeKLists {
         return head.next;
     }
 
+    /*
+    模仿归并排序的思想，先分再合。
+     */
+
 
     public ListNode mergeKLists1(ListNode[] lists) {
-
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
 
     }
 
-    private ListNode merge(ListNode[] lists, int left, int right){
-        if(left == right){
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) {
             return lists[left];
         }
-        int mid = left + (right - left)/2;
-
+        int mid = left + (right - left) / 2;
+        ListNode node1 = merge(lists, left, mid);
+        ListNode node2 = merge(lists, mid + 1, right);
+        return merge(node1, node2);
 
 
     }
 
+    private ListNode merge(ListNode node1, ListNode node2) {
+        if (node1 == null || node2 == null) {
+            return node1 == null ? node2 : node1;
+        }
+        if (node1.val < node2.val) {
+            node1.next = merge(node1.next, node2);
+            return node1;
+        } else {
+            node2.next = merge(node1, node2.next);
+            return node2;
+        }
 
-
-
+    }
 
 
 }
