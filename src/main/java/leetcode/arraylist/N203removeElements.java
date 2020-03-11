2424161e7e7703f4ed34f846967476ef0d70730e
Qunsong Fan (qufan) <qufan@cisco.com>
2020-03-11 10:54:31 +0800

    digui

---
+++ src/main/java/leetcode/arraylist/N203removeElements.java
@@ -0,0 +1,50 @@
+package leetcode.arraylist;
+
+public class N203removeElements {
+    /*
+    删除链表中等于给定值 val 的所有节点。
+
+示例:
+
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5
+
+
+     */
+
+
+    /*
+    递归写法
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return head;
+        }
+        if(head.val == val){
+            return removeElements(head.next, val);
+        }else {
+            head.next = removeElements(head.next, val);
+        }
+        return head;
+
+    }
+
+
+    public ListNode removeElements1(ListNode head, int val) {
+        if(head == null){
+            return head;
+        }
+        ListNode res = new ListNode(-1);
+        ListNode tmp = res;
+        while (head != null){
+            if(head.val != val){
+                res.next = head;
+                res = res.next;
+            }
+            head = head.next;
+        }
+        res.next = null;
+        return tmp.next;
+
+    }
+}
--- src/main/java/leetcode/huadongchuangkou/N076MinWindow.java
+++ src/main/java/leetcode/huadongchuangkou/N076MinWindow.java
@@ -21,14 +21,56 @@ public class N076MinWindow {
      */
 
     public String minWindow(String s, String t) {
-        if(s == null || s == "" || t == null || t=="" || s.length()<t.length()){
+        if (s == null || s == "" || t == null || t == "" || s.length() < t.length()) {
             return "";
         }
-        int[] help = new int[256];
-        int[] window = new int[256];
+        //用来统计t中每个字符出现次数
+        int[] needs = new int[128];
+        //用来统计滑动窗口中每个字符出现次数
+        int[] window = new int[128];
 
+        for (int i = 0; i < t.length(); i++) {
+            needs[t.charAt(i)]++;
+        }
 
-        return "";
+        int left = 0;
+        int right = 0;
 
+        String res = "";
+
+        //目前有多少个字符
+        int count = 0;
+
+        //用来记录最短需要多少个字符。
+        int minLength = s.length() + 1;
+
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            window[ch]++;
+            if (needs[ch] > 0 && needs[ch] >= window[ch]) {
+                count++;
+            }
+
+            //移动到不满足条件为止
+            while (count == t.length()) {
+                ch = s.charAt(left);
+                if (needs[ch] > 0 && needs[ch] >= window[ch]) {
+                    count--;
+                }
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    res = s.substring(left, right + 1);
+
+                }
+                window[ch]--;
+                left++;
+
+            }
+            right++;
+
+        }
+        return res;
     }
+
+
 }
